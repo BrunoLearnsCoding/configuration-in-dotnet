3cd629ee2f1038a9ad10adafcdc6d315baca0997
brunolearnscoding <bruno@ilearncoding.com>
2024-02-10 11:21:39 +0330

    GetApplicationEnvironment() added
    The application environment could be set by an environment variable
    and/or by a console argument. The method finds the values. The console
    argument has precedenc and will determines the application environment
    if it exists.

--- console-template/Program.cs
+++ console-template/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
 namespace console_template;
 
@@ -13,15 +14,42 @@ class Program
 
     private static IConfigurationRoot BuildConfiguration(string[] args)
     {
-        var env = Environment.GetEnvironmentVariable("Environment");
-        System.Console.WriteLine(env);
+        var env = GetApplicationEnvironment(args);
         var builder = new ConfigurationBuilder()
             .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
             .AddEnvironmentVariables();
-        
+
         return builder.Build();
 
+    }
+    /// <summary>
+    /// To specify the environment of the application, we should see if there is an 
+    /// environemnt variable and/or a parameter which comes from console arguments 
+    /// named '--Environment. The console argument overrides the environment variable
+    /// and then the ending result returns.
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns>string?</returns>
+    private static string? GetApplicationEnvironment(string[] args)
+    {
+        var env = Environment.GetEnvironmentVariable("Environment");
+        System.Console.WriteLine(env);
+        bool found = false;
+        foreach (var item in args)
+        {
+            if (found == true) {
+                env = item;
+                break;
+            }
+            if (item.ToLower() == "--environment")
+            {
+                found = true;
+            }
+        }
+        System.Console.WriteLine(env);
+
+        return env;
     }
 }
